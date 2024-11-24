f84675e4f05c107e6a80733f2ed2823b8a1a8e3d
Silas <silas.progamacao@gmail.com>
2024-11-24 13:46:58 -0300

    new commit

---
+++ libs/tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        // Paleta de cores principal
+        bgGray: '#f3f3f3',
+        bgHeader: '#FFFAEC',
+        breadCrust: '#D4A373', // Casca de pão (background principal)
+        softCream: '#FFF8E1',
+        softCreamHover: '#fcefc6',
+        goldenButter: '#FFD54F', // Manteiga dourada (destaques)
+        chocolateBrown: '#6D4C41', // Marrom chocolate (textos principais)
+        berryRed: '#D84315', // Vermelho berry (chamadas para ação)
+        warmGray: '#BCAAA4', // Cinza quente (detalhes ou textos neutros)
+        bottonColor: '#C6713A',
+        // Cores adicionais para estados
+        hoverGolden: '#FFC107', // Amarelo mais vibrante para hover
+        activeRed: '#BF360C', // Vermelho escuro para cliques
+        subtleCream: '#FFFDE7', // Um creme quase branco para seções secundárias
+        shadowGray: '#8D6E63' // Cinza marrom para sombras e bordas
+      }
+    }
+  }
+}
